319d7d4cf68cbd5b4f293d20c7bec94a207a177a
Sergio Leon <leonsergio0011@gmail.com>
2019-06-10 02:24:51 +0200

    Learning basic CRUD operations

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,13 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+const ObjectID = mongodb.ObjectID;
 
 const connectionURL = 'mongodb://localhost:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if(error){
@@ -12,7 +17,54 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   const db = client.db(databaseName);
 
   db.collection('users').insertOne({
-    name: 'Leon',
-    age: '28'
+    name: 'Blas',
+    age: '41'
+  }, (error, result) => {
+    if(error){
+      return console.log("Unable to insert user");
+    }
+
+    console.log(result.ops);
   });
+
+
+/*  db.collection('users').insertMany([
+    {
+        name: 'Jen',
+        age: 28
+    },
+    {
+        name: 'Gunther',
+        age: 27
+    }
+  ], (error, result) => {
+    if(error){
+      return console.log("Unable to insert documents");
+    }
+
+    console.log(result.ops);
+  }); */
+
+  /*db.collection('tasks').insertMany([
+    {
+      description: 'Learn nodeJS',
+      completed: true
+    },
+    {
+      description: 'Learn React',
+      completed: false
+    },
+    {
+      description: 'Find a good job',
+      completed: false
+    }
+  ], (error, result) => {
+    if(error){
+      return console.log('Unable to insert documents');
+    }
+
+    console.log(result.ops);
+  });*/
+
+
 });
