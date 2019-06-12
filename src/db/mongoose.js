efcaad373631e5735b524d33429588e5c0149c78
Sergio Leon <leonsergio0011@gmail.com>
2019-06-13 01:13:15 +0200

    Mongoose and validator library

---
+++ src/db/mongoose.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+mongoose.connect('mongodb://localhost:27017/task-manager-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+     type: String,
+     required: true,
+     trim: true,
+     lowercase: true,
+     validate(value){
+       if(!validator.isEmail(value)){
+         throw new Error('Email is invalid');
+       }
+     }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value){
+      if(value < 0){
+        throw new Error('Age must be a positive number');
+      }
+    }
+  }
+});
+
+const me = new User({
+  name: '    Leon',
+  email: 'pop@adf.com'
+});
+
+me.save()
+  .then((result) => {console.log(result)})
+  .catch((error) => {console.log(error)})
+
+  /*const Task = mongoose.model('Task', {
+    description: {
+      type: String
+    },
+    completed: {
+      type: Boolean
+    }
+  });
+
+  const firstPriority = new Task({
+    description: "Learning react fast",
+    completed: false
+  });
+
+  firstPriority.save()
+    .then((result) => {console.log(result)})
+    .catch((error) => {console.log(error)})
+*/
