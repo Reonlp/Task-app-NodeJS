4584ad0baf287e92c2cc31641315aacd387e75d3
Sergio Leon <leonsergio0011@gmail.com>
2019-07-06 05:48:58 +0200

    Continuing with the upload profile pic section

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const sharp = require('sharp');
 const router  = new express.Router();
 
 
@@ -119,7 +120,6 @@ router.patch('/users/me', auth, async (req, res) => {
 
 
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -132,11 +132,35 @@ const upload = multer({
   }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+  req.user.avatar = buffer;
+  await req.user.save();
   res.send();
 }, (error, req, res, next) => {
   res.status(400).send({ message: message.error });
 });
 
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user  || !user.avatar){
+      throw new Error();
+    }
+
+    res.set('Content-Type', 'image/png');
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
 
 module.exports = router;
